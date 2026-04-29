b4495fb172f05a0fd1ecc505f8e24e5a939008c1
Shalon Fernando <shalonfernando52@gmail.com>
2026-04-29 10:46:22 +0530

    fix: export

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Greeting from './components/Greeting'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
       <NavBar />
       <Greeting />
+      <Footer />
     </div>
   )
 }
