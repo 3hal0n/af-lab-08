1ee38a7d3cabbd140b032c763037ce2f8d6fbdfa
Shalon Fernando <shalonfernando52@gmail.com>
2026-04-29 10:38:32 +0530

    ref: greeting with tailwind css

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-function Greeting() {
+const Greeting = () => {
   return (
-    <div>
-        <h1 className='text-blue-500'>Hello World!</h1>
+    <div className="flex flex-col items-center justify-center py-20 px-4">
+      <h1 className="text-5xl font-black tracking-tighter text-slate-900 md:text-7xl">
+        Hello <span className="bg-gradient-to-r from-blue-600 to-cyan-500 bg-clip-text text-transparent">World.</span>
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Greeting
+export default Greeting;
